e6cf7dd0732795259d696b0e77e72916157fac66
username10000 <zvladmihail@yahoo.com>
2018-03-13 10:59:53 +0000

    Final Changes

--- Assets/LookAtLeader.cs
+++ Assets/LookAtLeader.cs
@@ -6,16 +6,23 @@ public class LookAtLeader : MonoBehaviour {
 
     public GameObject leader;
 
+    public Vector3 target;
+    public Vector3 newTarget;
+
 	// Use this for initialization
 	void Start () {
         GameObject[] objs = GameObject.FindGameObjectsWithTag("Leader");
         if (objs.Length > 0) {
             leader = objs[0];
         }
+        target = leader.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.LookAt(leader.transform.position, Vector3.up);
+        // Look at leader
+        newTarget = leader.transform.position;
+        target = Vector3.Lerp(target, newTarget, Time.deltaTime);
+        transform.LookAt(target, Vector3.up);
 	}
 }
